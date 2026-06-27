855ddfa2aa2c270888799da8ffd36cefddfc5b36
Rohak <rohak1210@gmail.com>
2026-06-27 21:33:17 +0530

    post creation

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 const userModel = require("../models/user.model")
-const crypto = require("crypto")
+const bcrypt = require("bcryptjs")
 const jwt = require('jsonwebtoken')
 
 
@@ -20,7 +20,8 @@ async function registerController(req,res){
         })
     }
 
-    const hash = crypto.createHash('sha256').update(password).digest('hex')
+    const hash = await bcrypt.hash(password, 10)
+
 
     const user = await userModel.create({
         username,
@@ -69,9 +70,7 @@ async function loginController (req,res){
         })
     }
 
-    const hash = crypto.createHash("sha256").update(password).digest('hex')
-
-    const isPasswordValid = hash == user.password
+    const isPasswordValid = await bcrypt.compare(password, user.password)
 
     if(!isPasswordValid){
         return res.status(401).json({
@@ -96,6 +95,10 @@ async function loginController (req,res){
         }
     })
 }
+
+
+
+
 
 module.exports = {
     registerController,
