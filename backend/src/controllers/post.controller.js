ccb1927c8182fda77f5cd8d2c64160a2689eb95f
Rohak <rohak1210@gmail.com>
2026-07-01 16:27:12 +0530

    more frontend

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -108,8 +108,20 @@ async function likePostController(req, res) {
     })
 }
 
-async function getFeedController(req,res){
-    const posts = await postModel.find().populate("user")
+async function getFeedController(req, res) {
+    const user = req.user
+
+    const posts = await Promise.all(
+        (await postModel.find().populate("user").lean())
+        .map(async (post) => {
+            const isLiked = await likeModel.findOne({
+                user: user.username,
+                post: post._id
+            })
+            post.isLiked = !!isLiked  
+            return post                           
+        })
+    )
 
     res.status(200).json({
         message: "posts fetched succesfully",
--- frontend/src/features/post/pages/Feed.jsx
+++ frontend/src/features/post/pages/Feed.jsx
@@ -1,35 +1,32 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import "../style/feed.scss"
+import Post from '../components/Post'
+import { usePost } from '../hook/usePost'
 
 const Feed = () => {
+
+    const {feed,handleGetFeed,loading} = usePost()
+    useEffect(()=>{
+        handleGetFeed()
+    },[])
+
+    if(loading || !feed){
+        return (<main><h1>Feed is Loading!!!</h1></main>)
+    }
+
+    console.log(feed)
+
   return (
-      <main className='feed-page' >
-          <div className="feed">
-              <div className="posts">
-                  <div className="post">
-                      <div className="user">
-                        <div className="img-wrapper">
-                            <img src="https://images.unsplash.com/photo-1640951613773-54706e06851d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D" alt="" />
-                        </div>
-                          <p>Username</p>
-                      </div>
-                      <img src="https://plus.unsplash.com/premium_photo-1661306510847-c1abc605a7b4?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZG9vbXxlbnwwfHwwfHx8MA%3D%3D" alt="" />
-                      <div className="icons">
-                        <div className="left">
-                            <button></button>
-                            <button></button>
-                            <button></button>
-                        </div>
-                        <div className="right"></div>
-                      </div>
-                      <div className="bottom">
-                          <p className="caption">caption caption</p>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </main>
-  )
+    <main className='feed-page'>
+        <div className="feed">
+            <div className="posts">
+                {feed.map(post => (
+                    <Post user={post.user} post={post} />
+                ))}
+            </div>
+        </div>
+    </main>
+)
 }
 
 export default Feed
